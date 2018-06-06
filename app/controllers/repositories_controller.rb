57a377ce722cae37c9ab86d5fc2966b2dad0441b
Randall Robinson <keith_adams007@yahoo.com>
2018-06-06 20:23:28 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -9,7 +9,6 @@ class RepositoriesController < ApplicationController
     @resp = Faraday.get 'https://api.github.com' do |req|
         req.params['client_id'] = Iv1.ef7b896962619bae
         req.params['client_secret'] = f9874efb89be0cbabff97f432c7b8f86752245d6
-        req.params['v'] = '20160201'
         req.params['query'] = params[:query]
        
       end
