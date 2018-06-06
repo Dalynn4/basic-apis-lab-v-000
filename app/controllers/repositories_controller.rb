c5461c175cd67c0e8f8a78244c74165cce6cbc1a
Randall Robinson <keith_adams007@yahoo.com>
2018-06-06 20:33:01 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -6,7 +6,7 @@ class RepositoriesController < ApplicationController
 
   def github_search
     raise.inspect
-       @resp = Faraday.get 'https://api.github.com' do |req|
+       @resp = Faraday.get 'https://api.github.com/search/repositories?q=' do |req|
         req.params['client_id'] = 'Iv1.ef7b896962619bae'
         req.params['client_secret'] = 'f9874efb89be0cbabff97f432c7b8f86752245d6'
         req.params['query'] = params[:query]
